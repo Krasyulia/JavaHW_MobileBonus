aac2b2ed0cf13aee4783cc64f3392b89628c37b0
Yulia <krasjulia@gmail.com>
2022-02-14 22:46:39 +0400

    version1

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+      int startBalance = 500; //стартовый баланс
+      int increaseSum = 3330; //сумма пополнения
+      int bonus = increaseSum > 1000 ? increaseSum / 100 : 0; //расчет бонуса
+      int finalBalance = startBalance + increaseSum + bonus; //расчет общей суммы на балансе
+
+        System.out.println("Итоговый баланс: " + finalBalance);
+        System.out.println("Бонусные рубли: " + bonus);
+
+    }
+
+}
