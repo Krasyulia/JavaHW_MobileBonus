9603a20e38bfd2147f73f836e414f9f7c3302df9
Yulia <krasjulia@gmail.com>
2022-02-19 02:00:41 +0400

    ReformatCode

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-      int startBalance = 500; //стартовый баланс
-      int increaseSum = 3330; //сумма пополнения
-      int bonus = increaseSum > 1000 ? increaseSum / 100 : 0; //расчет бонуса
-      int finalBalance = startBalance + increaseSum + bonus; //расчет общей суммы на балансе
+        int startBalance = 500; //стартовый баланс
+        int increaseSum = 3330; //сумма пополнения
+        int bonus = increaseSum > 1000 ? increaseSum / 100 : 0; //расчет бонуса
+        int finalBalance = startBalance + increaseSum + bonus; //расчет общей суммы на балансе
 
         System.out.println("Итоговый баланс: " + finalBalance);
         System.out.println("Бонусные рубли: " + bonus);
